dfc1dd18196448eefcceb49ad897e3d1901149b7
yapelenua <kbshnik2020@gmail.com>
2024-03-12 23:27:07 +0200

    minor fixes

--- src/pages/MapComponent.tsx
+++ src/pages/MapComponent.tsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 import React, { useState, useEffect } from 'react';
 import GoogleMapReact from 'google-map-react';
 import firebase from 'firebase/compat/app';
